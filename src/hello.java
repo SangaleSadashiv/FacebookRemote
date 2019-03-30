626fe6e05a6bcf584c159be2c22769a417348c09
SangaleSadashiv <sgsangale19@gmail.com>
2019-03-30 15:00:09 +0530

    my second commit

--- src/hello.java
+++ src/hello.java
@@ -4,6 +4,7 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("welcome sadashiv");
+		System.out.println("github welcoming you");
 
 	}
 
